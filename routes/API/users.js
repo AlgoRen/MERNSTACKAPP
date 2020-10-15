b6055edcb52d2989f2a9c1141622b35a5b8738ed
Algo Ren <algorenstudio@gmail.com>
2020-10-15 14:05:29 -0400

    Created Profile Schema with user information such as skills, location, website, exerpience, social media, etc. Created post route for login auth.

--- routes/API/users.js
+++ routes/API/users.js
@@ -73,9 +73,8 @@ async (req, res) => {
     } catch(err) {
         console.error(err.message);
         res.status(500).send('Server error')
+        }
     }
-
-
-});
+);
 
 module.exports = router;
