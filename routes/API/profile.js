08265b659072db2c1c41606be9ef6da234276ac1
Algo Ren <algorenstudio@gmail.com>
2020-10-21 17:47:32 -0400

    Added routes to delete user profile, get all profiles, get a profile by user ID, add an experience to a profile, delete an experience from a profile.

--- routes/API/profile.js
+++ routes/API/profile.js
@@ -65,7 +65,7 @@ router.post(
       linkedin,
     } = req.body;
 
-    // * Build Profile Object *
+    // * Build profile object *
     const profileFields = {};
     profileFields.user = req.user.id;
     if (company) profileFields.company = company;
@@ -113,5 +113,135 @@ router.post(
     // res.send("Hello");
   }
 );
+
+// @route   GET API/profile/
+// @desc    Get all profiles
+// @access  Public
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   GET API/profile/user/:user_id
+// @desc    Get profile by ID
+// @access  Public
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate("user", ["name", "avatar"]);
+    console.log(
+      profile,
+      "\nConsole log on mongoose search of database for user id."
+    );
+    if (!profile) return res.status(400).json({ msg: "Profile not found." });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == "ObjectId") {
+      return res.status(400).json({ msg: "Profile not found." });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   DELETE API/profile/user/:user_id
+// @desc    Delete profile, user & posts
+// @access  Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    // TODO - Remove users posts
+    // * Remove profile *
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // * Remove user *
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: "User deleted." });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   PUT API/profile/experience
+// @desc    Add profile experience
+// @access  Private
+router.put(
+  "/experience",
+  [
+    auth,
+    [
+      check("title", "Title is required").not().isEmpty(),
+      check("company", "Company is required").not().isEmpty(),
+      check("from", "From date is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+      const errors = validationResult(req);
+      if(!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() })
+      }
+
+      const {
+          title,
+          company,
+          location,
+          from,
+          to,
+          current,
+          description
+      } = req.body;
+
+      const newExp = {
+          title,
+          company,
+          location,
+          from,
+          to,
+          current,
+          description
+      }
+      // * Dealing with MongoDB *
+      try {
+          const profile = await Profile.findOne({ user: req.user.id });
+
+          profile.experience.unshift(newExp);
+
+          await profile.save();
+
+          res.json(profile);
+      } catch (err) {
+          console.error(err.message);
+          res.status(500).send('Server Error');
+      }
+  }
+);
+
+// @route   DELETE API/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        // * Get remove index *
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+        profile.experience.splice(removeIndex, 1);
+
+        await profile.save();
+
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
 
 module.exports = router;
