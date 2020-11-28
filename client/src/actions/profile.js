e82a28c039b9d2477990143d51b0e9b3a78259f3
Algo Ren <algorenstudio@gmail.com>
2020-11-27 20:30:14 -0500

    Created the createProfile action that takes in formData. Our createProfile action uses the parameters history and edit as well. History is being used as a way to redirect a user who has created a profile for the first time. While edit parameter is being set as false by default in order to distinguish between a user who is creating a profile for the first time and a user who is updating their profile. If the user is only editing a profile he or she will not be redirected to the dashboard. After getting a response from our axios.post call to /api/profile we dispatch our getProfile reducer with the data from our post request and then dispatch a setAlert reducer with either Profile Updated or Profile Created depending on the edit parameter... I learned a lot coding this out, I feel a stronger understanding on how React is used with handling data and updating the page.

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { format } from "prettier";
 import { setAlert } from "./alert";
 
 import { GET_PROFILE, PROFILE_ERROR } from "./types";
@@ -15,6 +16,47 @@ export const getCurrentProfile = () => async (dispatch) => {
       payload: res.data,
     });
   } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Create or update profile
+//* Using the edit parameter to distinguish between a user creating a profile
+//* versus a user who is updating a profile in order to know where to
+//* redirect them on completion. Using history.push to send user to dashboard page.
+
+export const createProfile = (formData, history, edit = false) => async (
+  dispatch
+) => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const res = await axios.post("/api/profile", formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+
+    dispatch(setAlert(edit ? "Profile Updated" : "Profile Created", "success"));
+
+    if (!edit) {
+      history.push("/dashboard");
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+    }
+
     dispatch({
       type: PROFILE_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status },
