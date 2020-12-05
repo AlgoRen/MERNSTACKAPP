35525434b5007d54cfbeae2a0c30fd00482def07
Algo Ren <algorenstudio@gmail.com>
2020-12-04 20:06:59 -0500

    Created profile actions and reducers to getProfiles, which gets all profiles in DB. Along with a getProfileById that gets a specific user's profile by its user id (not profile id). Finally created a getGitHubRepos action that gets all repos by that user so we pass in the user's github username.

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -4,10 +4,12 @@ import { setAlert } from "./alert";
 
 import {
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_ERROR,
   UPDATE_PROFILE,
   CLEAR_PROFILE,
   DELETE_ACCOUNT,
+  GET_REPOS,
 } from "./types";
 
 // Get current users profile
@@ -19,6 +21,61 @@ export const getCurrentProfile = () => async (dispatch) => {
 
     dispatch({
       type: GET_PROFILE,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Get all profiles
+export const getProfiles = () => async (dispatch) => {
+  dispatch({ type: CLEAR_PROFILE });
+  try {
+    // Get's current user's profile from our backend that has token.
+    const res = await axios.get("api/profile");
+
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Get profile by ID
+export const getProfileById = (userId) => async (dispatch) => {
+  try {
+    // Get's a user's profile from the user ID (not the profile ID.
+    const res = await axios.get(`api/profile/user/${userId}`);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Get Github repos
+export const getGithubRepos = (username) => async (dispatch) => {
+  try {
+    // Get's users Github repos.
+    const res = await axios.get(`api/profile/github/${username}`);
+
+    dispatch({
+      type: GET_REPOS,
       payload: res.data,
     });
   } catch (err) {
