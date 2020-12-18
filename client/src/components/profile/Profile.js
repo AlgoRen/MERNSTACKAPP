e8f0d752ecec5d0d394880eaf7952efc6b2dba50
Algo Ren <algorenstudio@gmail.com>
2020-12-17 23:10:55 -0500

    Created another simple component, ProfileAbout. ProfileAbout returns the selected profile bio, skills, and name. In order to get all our skills we map through skills array using the index as a key for each div we create.

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import ProfileTop from "./ProfileTop";
+import ProfileAbout from "./ProfileAbout";
 import { getProfileById } from "../../actions/profile";
 
 const Profile = ({
@@ -36,6 +37,7 @@ const Profile = ({
             )}
           <div class="profile-grid my-1">
             <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
           </div>
         </Fragment>
       )}{" "}
