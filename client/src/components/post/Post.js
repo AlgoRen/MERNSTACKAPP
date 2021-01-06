c5a8ab17f5b1cd12c515f68c0c77450c205ac00f
Algo Ren <algorenstudio@gmail.com>
2021-01-06 17:21:55 -0500

    Created the CommentForm for posting a comment and addComment and deleteComment actions and reducers. The code used is very similar to what was used in add and deleting posts. One thing to note is that the individual Post componenet uses the PostItem componenet used in the displaying of Posts, the displaying of all post. The PostItem component in the Post componenet disables the actions (does not show them) by using defaultProps and setting it to false passing it into the PostItem component.

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -2,15 +2,27 @@ import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
+import PostItem from "../posts/PostItem";
+import CommentForm from "../post/CommentForm";
 import { getPost } from "../../actions/post";
+import { Link } from "react-router-dom";
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
     getPost(match.params.id);
   }, [getPost]);
 
-  return <div>Post</div>;
-  //   Testing...
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <Link to="/posts" className="btn">
+        Back To Posts
+      </Link>
+      <PostItem post={post} showActions={false} />;
+      <CommentForm postId={post._id} />
+    </Fragment>
+  );
 };
 
 Post.propTypes = {
