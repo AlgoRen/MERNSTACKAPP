9b9f17c1b10bc32af0ab2c7e84c883a8b1230893
Algo Ren <algorenstudio@gmail.com>
2021-01-13 13:05:59 -0500

    Completed commenting Post.js in the post folder... Now onto the posts folder.

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -8,6 +8,30 @@ import CommentItem from "../post/CommentItem";
 import { getPost } from "../../actions/post";
 import { Link } from "react-router-dom";
 
+// Purpose:
+//    1) To display an individual page for posts that consist of the post, comment form, and all
+// comments left under the post.
+// How it works:
+//    1) The Post function takes in getPost and match, along with post and loading that is deconstructed
+// off of the post object.
+//    2) The Post function uses useEffect to call an anonymous function that executes the getPost function
+// with match.params.id as a parameter. getPost is the dependency of useEffect.
+//    3) The Post function returns the Spinner fragment, brought in from layout, if loading or post is equal
+// to null. Else a Fragment with a Link to posts, PostItem, CommentForm, and CommentItem fragments is returned.
+//    4) The Link tag displays a button that once clicked brings user to /posts, where they can see every post.
+//    5) The PostItem fragment displays the individual post by setting a prop named post to the post state.
+// A showActions props is set to the value of false in order for us to reuse to PostItem fragment that is
+// created in the posts folder, but without having actions such as like, dislike, and delete, be displayed.
+//    6) The CommentForm fragment displays the commentForm passing in a prop named postId with the value
+// post._id that we get from the post state.
+//    7) In a div called comments there is a map function attached to the array of post.comments. The map
+// function takes in comment for its individual instance and returns the CommentItem fragment using a key with
+// the value of comment._id, a prop with the name comment has the value of comment, and a prop with the name
+// postId has the value of post._id
+//    8) getPost and post are defined in Post.propTypes. getPost is defined as a required function. The post
+// state is defined as a required object.
+//    9) mapStateToProps defines the post state using state.post
+
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
     getPost(match.params.id);
