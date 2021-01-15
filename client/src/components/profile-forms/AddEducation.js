e23a78957a058ed33c6ef6db51c029c429de70da
Algo Ren <algorenstudio@gmail.com>
2021-01-14 23:54:51 -0500

    Completed commenting AddEducation.js and AddExperience.js files in profile-forms folder.

--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -1,8 +1,32 @@
 import React, { Fragment, useState } from "react";
-import { withRouter } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { addEducation } from "../../actions/profile";
+
+// Purpose:
+//    1) To create a form that provides a user with the ability to add education credentials.
+
+// How it works:
+//    1) The AddEducation function takes in addEducation action and the history prop.
+//    2) The AddEducation function calls a useState hook and sets properties of school, degree, fieldofstudy,
+// from, to, and description to a empty string, and sets current to false. The useState is deconstructed to
+// an object called formData and function called setFormData.
+//    3) The AddEducation function calls another useState hook passing in false and deconstructs a boolean
+// called toDateDisabled and a function named toggleDisabled off of the useState.
+//    4) The following constants are deconstructed off of the formData object: school, degree, fieldofstudy,
+// from, to, current, and description.
+//    5) A onChange function is defined passing in e (the event object) that returns setFormData calling it
+// with the following parameters: the entire state of formData and the Key-Value pair e.target.name: e.target.value
+// inside of an object.
+//    6) The AddEducation function returns a Fragment that contains a h1 tag saying Add An Education, a p tag
+// saying Add any school or bootcamp that you have attended, a small tag saying * = required field, a form tag
+// that contains inputs for school, degree, fieldofstudy, from, current, to, and a textarea for description.
+// Also, a Link fragment that will take a user to the dashboard route. Lastly, an input tag with the type of
+// submit that once clicked will trigger the onSubmit property on form.
+//   7) When the form is submitted, the onSubmit property calls an anonymous function that passes in e,
+// preventsDefault, and executes the addEducation action with the parameters formData and history.
+//   8) The addEducation action is defined as a required function in AddEducation.propTypes.
 
 const AddEducation = ({ addEducation, history }) => {
   const [formData, setFormData] = useState({
@@ -119,9 +143,9 @@ const AddEducation = ({ addEducation, history }) => {
           ></textarea>
         </div>
         <input type="submit" class="btn btn-primary my-1" />
-        <a class="btn btn-light my-1" href="dashboard.html">
+        <Link class="btn btn-light my-1" to="/dashboard">
           Go Back
-        </a>
+        </Link>
       </form>
     </Fragment>
   );
--- client/src/components/profile-forms/AddExperience.js
+++ client/src/components/profile-forms/AddExperience.js
@@ -1,8 +1,32 @@
 import React, { Fragment, useState } from "react";
-import { withRouter } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { addExperience } from "../../actions/profile";
+
+// Purpose:
+//    1) To create a form that provides a user with the ability to add work experience.
+
+// How it works:
+//    1) The AddExperience function takes in addExperience action and the history prop.
+//    2) The AddExperience function calls a useState hook and sets properties of company, title, location,
+// from, to, and description to an empty string, and sets current to false. The useState is deconstructed to
+// an object called formData and function called setFormData.
+//    3) The AddExperience function calls another useState hook passing in false and deconstructs a boolean
+// called toDateDisabled and a function named toggleDisabled off of the useState.
+//    4) The following constants are deconstructed off of the formData object: company, title, location,
+// from, to, current, and description.
+//    5) A onChange function is defined passing in e (the event object) that returns setFormData calling it
+// with the following parameters: the entire state of formData and the Key-Value pair e.target.name: e.target.value
+// inside of an object.
+//    6) The AddExperience function returns a Fragment that contains a h1 tag saying Add An Experience, a p tag
+// saying Add any developer/programming positions that you have had in the past, a small tag saying * = required field,
+// a form tag that contains inputs for title, company, location, from, current, to, and a textarea for description.
+// Also, a Link fragment that will take a user to the dashboard route. Lastly, an input tag with the type of
+// submit that once clicked will trigger the onSubmit property on form.
+//   7) When the form is submitted, the onSubmit property calls an anonymous function that passes in e,
+// preventsDefault, and executes the addExperience action with the parameters formData and history.
+//   8) The addExperience action is defined as a required function in AddExperience.propTypes.
 
 const AddExperience = ({ addExperience, history }) => {
   const [formData, setFormData] = useState({
@@ -111,9 +135,9 @@ const AddExperience = ({ addExperience, history }) => {
           ></textarea>
         </div>
         <input type="submit" class="btn btn-primary my-1" />
-        <a class="btn btn-light my-1" href="dashboard.html">
+        <Link class="btn btn-light my-1" to="/dashboard">
           Go Back
-        </a>
+        </Link>
       </form>
     </Fragment>
   );
