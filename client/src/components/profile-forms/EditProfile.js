ccb569a887335c89cdfb1d8d2a3ec4b2e9f7b804
Algo Ren <algorenstudio@gmail.com>
2021-01-15 21:18:15 -0500

    Completed commenting all of the profile-forms folder. The comments in this folder was some of the most lengthy thus far.

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -4,6 +4,46 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { createProfile, getCurrentProfile } from "../../actions/profile";
 
+// Purpose:
+//     1) To create a form that allows a user to edit an existing profile.
+
+// How it works:
+//     1) The EditProfile function takes in profile and loading that are deconstructed off of the profile
+// state, createProfile and getCurrentProfile actions, and the history prop.
+//     2) The EditProfile function calls a useState hook passing in false and deconstructs a boolean
+// called toDateDisabled and a function named toggleDisabled off of the useState.
+//     3) The EditProfile function calls a useState hook that calls the getCurrentProfile action that updates
+// the profile state. We then call setFormData to set the formData. If the following state is loading or the
+// property on the profile object does not exist we are then setting the property to an empty string,
+// else we are setting the property to what we find on the profile state. The useState hook has loading
+// and getCurrentProfile as dependencies.
+//     4) The following constants are deconstructed off of the formData object: company, website, location,
+// status, skills, githubusername, bio, twitter, facebook, linkedin, youtube, instagram.
+//     5) A onChange function is defined passing in e (the event object) that returns setFormData calling it
+// with the following parameters: the entire state of formData and the Key-Value pair e.target.name: e.target.value
+// inside of an object.
+//     6) The EditProfile function returns a Fragment that contains h1 tag saying Edit Your Profile, a p tag
+// saying Let's get some information to make your profile stand out, a small tag saying * = required field, and
+// a form tag.
+//     7) The form tag contains a select tag that has the following options: * Select Professional Status (shown
+// by default), Developer, Junior Developer, Senior Developer, Manager, Student or Learning, Instructor or Teacher,
+// Intern, Other. The value attribute of the select tag is set to the status constant. The onChange attribute of
+// the select tag is set to an anonymous function that passes in e and calls the onChange function.
+//     8) The form tag contains a div tag that contains inputs for company, website, location, skills, githubusername,
+// bio. All inputs have an onChange attribute that calls the onChange function; with the website input having its
+// value set to the urlToRender function passing in website as a parameter. Underneath the inputs is a button tag
+// with an onClick attribute assigned to an anonymous function that calls a function called toggleSocialInputs and
+// passing in !displaySocialInputs (meaning whatever the opposite value of the current state of displaySocialInputs),
+// effectively making it into a toggle button for showing or hiding the social media input fields.
+//     9) A JSX element checks displaySocialInputs, and if it contains a truth value, it will return a Fragment containing
+// input fields for twitter, facebook, youtube, linkedin, instagram. All of their values are set to constants with
+// the same as their name attribute and onChange attributes are set to anonymous functions that call onChange function.
+// Also, a Link fragment that will take a user to the dashboard route. Lastly, an input tag with the type of
+// submit that once clicked will trigger the onSubmit property on the form.
+//     10) The createProfile and getCurrentProfile actions are defined as a required function in EditProfile.propTypes.
+// The profile state is defined as required object. mapStateToProps defines profile using state.profile and auth using
+// state.auth.
+
 const urlToRender = (link) => {
   if (!link.match(/^[a-zA-Z]+:\/\//)) {
     return "//" + link;
@@ -37,7 +77,7 @@ const EditProfile = ({
   useEffect(() => {
     getCurrentProfile();
     // Filling the form with the current data.
-    //* If it is loading or the property on the profile object is null
+    //* If it is loading or the property on the profile object doesn't exist
     //* we are then setting the property to an empty string,
     //* else we are setting the property to what we find on the profile state.
     setFormData({
@@ -122,7 +162,7 @@ const EditProfile = ({
             type="text"
             placeholder="Website"
             name="website"
-            value={website}
+            value={urlToRender(website)}
             onChange={(e) => onChange(e)}
           />
           <small className="form-text">
